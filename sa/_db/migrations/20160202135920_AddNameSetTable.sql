c8ff82a5d6e7263fa1cd01140544e6716a5c601a
Roland Shoemaker <rolandshoemaker@gmail.com>
2016-02-08 14:47:50 -0800

    Consistent SQL type casing

--- sa/_db/migrations/20160202135920_AddNameSetTable.sql
+++ sa/_db/migrations/20160202135920_AddNameSetTable.sql
@@ -3,7 +3,7 @@
 -- SQL in section 'Up' is executed when this migration is applied
 
 CREATE TABLE `nameSets` (
-       `id` int(11) NOT NULL AUTO_INCREMENT,
+       `id` INT(11) NOT NULL AUTO_INCREMENT,
        -- SHA256 hash of alphabetically sorted, lowercased, comma joined
        -- DNS names contained in a certificate
        `setHash` BINARY(32) NOT NULL,
